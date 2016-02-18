8bf8cd3b6132f4b9e92094326efebd233de347d3
Damian Farina <damianfarina@gmail.com>
2016-02-17 22:29:13 -0300

    Update 2016-02-18T01:29:10.716Z

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,31 +1,6 @@
 $(function() {
-  var menuItems, sections;
-  $('#fullscreen-toggle').on('click', function() {
+  return $('#fullscreen-toggle').on("click", function() {
     return $('.container').toggleClass('fullscreen');
-  });
-  menuItems = $(".nav-links a");
-  sections = menuItems.map(function() {
-    var item;
-    item = $($(this).attr("href"));
-    if (item.length) {
-      return item;
-    }
-  });
-  return $('.content').on('scroll', function() {
-    var currentSection, sectionId, sectionsBelow, threshold, top, windowHeight;
-    top = $(this).scrollTop();
-    windowHeight = $(window).outerHeight();
-    threshold = top - windowHeight;
-    sectionsBelow = sections.map(function() {
-      var offset;
-      offset = $(this).offset();
-      if (offset.top < threshold) {
-        return this;
-      }
-    });
-    currentSection = sectionsBelow[sectionsBelow.length - 1] || sections[0];
-    sectionId = currentSection && currentSection.length ? currentSection.attr('id') : '';
-    return menuItems.parent().removeClass("active").end().filter("[href='#" + sectionId + "']").parent().addClass("active");
   });
 });
 
